33091424366aa73995a8849bf7314daecc1d4b94
Aleksandar Ljubic <aleksandar.ljbc@gmail.com>
2026-03-18 13:50:46 +0100

    Connect game loop with database and enable running the game

--- src/country_akinator/core.clj
+++ src/country_akinator/core.clj
@@ -1,31 +1,8 @@
 (ns country-akinator.core
   (:gen-class)
-  (:require [country-akinator.repository :as repo]
-            [country-akinator.db :as db]))
+  (:require [country-akinator.repository :as repository]
+            [country-akinator.play :as play]))
 
 (defn -main [& args]
-  (println "Country Akinator started.")
-  (println)
-
-  (println "Testing database connection...")
-  (println (db/test-connection))
-  (println)
-
-  (println "Loading countries from database...")
-  (println "Number of countries:" (count (repo/load-all-countries)))
-  (println)
-
-  (println "First 3 countries:")
-  (doseq [country (take 3 (repo/load-all-countries))]
-    (println country))
-  (println)
-
-  (println "Country with id 151:")
-  (println (repo/load-country-by-id 151))
-  (println)
-
-  (println "Checking map by id:")
-  (println (get (repo/load-countries-by-id) 151))
-  (println)
-
-  (println "Database read successful."))
+  (let [countries (repository/load-all-countries)]
+    (play/play-game countries)))
